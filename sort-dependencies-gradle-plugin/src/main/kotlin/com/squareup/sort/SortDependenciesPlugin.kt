3b3e2e6f62b19a92e4178fad17332c6da65f84ec
Zac Sweers <pandanomic@gmail.com>
2023-02-07 00:01:39 -0500

    Create sortDependencies configuration to allow customizing version
    Resolves #22. I couldn't get it working using just a version from an extension as Gradle doesn't seem to allow using a lazily-evaluated provider of a Dependency instance. Open to suggestions if there's another way! This instead uses a public `CONFIGURATION_NAME`-named configuration that has a default dependency of the embedded version, but can be replaced with the user's own version if they wish.
    
    ```kotlin
    dependencies {
      "sortDependencies"(some.newer.version)
    }
    ```

--- sort-dependencies-gradle-plugin/src/main/kotlin/com/squareup/sort/SortDependenciesPlugin.kt
+++ sort-dependencies-gradle-plugin/src/main/kotlin/com/squareup/sort/SortDependenciesPlugin.kt
@@ -6,22 +6,26 @@ import org.gradle.api.Project
 @Suppress("unused")
 class SortDependenciesPlugin : Plugin<Project> {
 
-  private companion object {
-    const val VERSION_FILENAME = "version.txt"
+  companion object {
+    private const val VERSION_FILENAME = "version.txt"
+    const val CONFIGURATION_NAME = "sortDependencies"
   }
 
   override fun apply(target: Project): Unit = target.run {
+    val version = SortDependenciesPlugin::class.java.classLoader.getResourceAsStream(VERSION_FILENAME)
+      ?.bufferedReader()
+      ?.use { it.readLine() }
+      ?: error("Can't find $VERSION_FILENAME")
+    val coordinates = "com.squareup:sort-gradle-dependencies-app:$version"
+    val configuration = configurations.create(CONFIGURATION_NAME)
+      .defaultDependencies {
+        it.add(dependencies.create(coordinates))
+      }
+
     tasks.register("sortDependencies", SortDependenciesTask::class.java) { t ->
       with(t) {
-        val version = javaClass.classLoader.getResourceAsStream(VERSION_FILENAME)
-          ?.bufferedReader()
-          ?.use { it.readLine() }
-          ?: error("Can't find $VERSION_FILENAME")
-        val coordinates = "com.squareup:sort-gradle-dependencies-app:$version"
-        val app = configurations.detachedConfiguration(dependencies.create(coordinates))
-
         buildScript.set(buildFile)
-        sortProgram.setFrom(app)
+        sortProgram.setFrom(configuration)
         mode.convention("sort")
       }
     }
