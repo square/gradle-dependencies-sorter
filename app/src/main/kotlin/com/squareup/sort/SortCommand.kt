4f640eeed0543dccd643056a933775beedfd5eae
Zac Sweers <pandanomic@gmail.com>
2023-02-07 14:11:48 -0500

    Port main merge change

--- app/src/main/kotlin/com/squareup/sort/SortCommand.kt
+++ app/src/main/kotlin/com/squareup/sort/SortCommand.kt
@@ -225,7 +225,7 @@ private fun logger(fileSystem: FileSystem, quiet: Boolean): DelegatingLogger {
     System.getProperty("java.io.tmpdir"),
     "dependencies-sorter"
   ).createDirectories()
-  val logFile = Files.createFile(logDir.resolve("${Instant.now()}.log"))
+  val logFile = Files.createFile(logDir.resolve("${Instant.now().toString().replace(":", "-")}.log"))
 
   return DelegatingLogger(
     delegate = LoggerFactory.getLogger("Sorter"),
