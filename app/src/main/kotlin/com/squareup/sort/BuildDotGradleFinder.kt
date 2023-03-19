4c41165479d6e25ba2949c7ef2596609dcc62ba1
Simon Marquis <contact@simon-marquis.fr>
2023-03-19 16:01:37 +0100

    Update `BuildDotGradleFinder` with a faster implementation of `FileTreeWalk`

--- app/src/main/kotlin/com/squareup/sort/BuildDotGradleFinder.kt
+++ app/src/main/kotlin/com/squareup/sort/BuildDotGradleFinder.kt
@@ -12,7 +12,7 @@ class BuildDotGradleFinder(
     // nb, if the path passed to the resolve method is already an absolute path, it returns that.
     .map { root.resolve(it).toFile() }
     .flatMap {
-      it.walk()
+      it.fastWalk()
         .filter(File::isBuildDotGradle)
         .map(File::toPath)
     }
@@ -24,6 +24,15 @@ class BuildDotGradleFinder(
       searchPaths: List<String>
     ): BuildDotGradleFinder = BuildDotGradleFinder(root, searchPaths)
   }
+}
+
+/**
+ * Gets a sequence for visiting this directory and all its content but skips `build` directories
+ * and directories that starts with a `.` like `.git`, `.gradle`, etc. for better performances
+ * as they generally don't contain Gradle build files, but a lot of uninteresting nested files and directories.
+ */
+private fun File.fastWalk(): Sequence<File> = walk().onEnter {
+  it.isDirectory && !it.name.startsWith(".") && it.name != "build"
 }
 
 private fun File.isBuildDotGradle(): Boolean {
