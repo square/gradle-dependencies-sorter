4251f653f97237281921d11591c4202f5e1e5ef0
Tony Robalik <trobalik@squareup.com>
2023-08-01 11:41:41 -0700

    Change resource name to avoid clobbering with other plugins.
    This could happen because the original name was very simply 'version.txt', which was also present from the Sonar plugin.
    Due to the nature of classloading, both this plugin's resource and the Sonar plugin's resource were available, and the
    latter was chosen instead. The simple solution was to namespace version.txt: it is now com-squareup-sort-version.txt.
    
    Resolves #17

--- sort-dependencies-gradle-plugin/src/main/kotlin/com/squareup/sort/SortDependenciesPlugin.kt
+++ sort-dependencies-gradle-plugin/src/main/kotlin/com/squareup/sort/SortDependenciesPlugin.kt
@@ -2,40 +2,48 @@ package com.squareup.sort
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
+import org.gradle.api.artifacts.Configuration
 import org.gradle.language.base.plugins.LifecycleBasePlugin
 
 @Suppress("unused")
 class SortDependenciesPlugin : Plugin<Project> {
 
   private companion object {
-    const val VERSION_FILENAME = "version.txt"
+    const val VERSION_FILENAME = "com-squareup-sort-version.txt"
   }
 
+  private lateinit var sortApp: Configuration
+
   override fun apply(target: Project): Unit = target.run {
+    sortApp = sortApp(this)
+
     tasks.register("sortDependencies", SortDependenciesTask::class.java) { t ->
       t.configure("sort", target)
     }
     val checkTask = tasks.register("checkSortDependencies", SortDependenciesTask::class.java) { t ->
       t.configure("check", target)
     }
+
     pluginManager.withPlugin("lifecycle-base") {
-      target.project.tasks.named(LifecycleBasePlugin.CHECK_TASK_NAME, Task::class.java).configure {
+      tasks.named(LifecycleBasePlugin.CHECK_TASK_NAME).configure {
         it.dependsOn(checkTask)
       }
     }
   }
 
   private fun SortDependenciesTask.configure(mode: String, project: Project) {
-      val version = javaClass.classLoader.getResourceAsStream(VERSION_FILENAME)
-        ?.bufferedReader()
-        ?.use { it.readLine() }
-        ?: error("Can't find $VERSION_FILENAME")
-      val coordinates = "com.squareup:sort-gradle-dependencies-app:$version"
-      val app = project.configurations.detachedConfiguration(project.dependencies.create(coordinates))
+    buildScript.set(project.buildFile)
+    sortProgram.setFrom(sortApp)
+    this.mode.set(mode)
+  }
 
-      buildScript.set(project.buildFile)
-      sortProgram.setFrom(app)
-      this.mode.set(mode)
+  private fun sortApp(project: Project): Configuration {
+    val version = javaClass.classLoader.getResourceAsStream(VERSION_FILENAME)
+      ?.bufferedReader()
+      ?.use { it.readLine() }
+      ?: error("Can't find $VERSION_FILENAME")
+    val coordinates = "com.squareup:sort-gradle-dependencies-app:$version"
+
+    return project.configurations.detachedConfiguration(project.dependencies.create(coordinates))
   }
 }
