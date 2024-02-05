3d028412dfcbf1526392a6fb8c14dd766f9433d0
Zac Sweers <pandanomic@gmail.com>
2024-02-05 00:08:05 -0500

    Syntax fix

--- sort-dependencies-gradle-plugin/src/main/kotlin/com/squareup/sort/SortDependenciesTask.kt
+++ sort-dependencies-gradle-plugin/src/main/kotlin/com/squareup/sort/SortDependenciesTask.kt
@@ -66,7 +66,7 @@ abstract class SortDependenciesTask @Inject constructor(
           addAll("--mode", mode)
           if (verbose) {
             add("--verbose")
-          )
+          }
         }
       }
     }
