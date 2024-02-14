dd01c930ef28f0174a990ead605a8b25513d4d49
Ivan Alvarado <ivalvara21@gmail.com>
2024-02-13 22:24:25 -0800

    Successfully getting version from appinfo.properties in jar

--- app/src/main/kotlin/com/squareup/sort/main.kt
+++ app/src/main/kotlin/com/squareup/sort/main.kt
@@ -4,7 +4,17 @@ import picocli.CommandLine
 import kotlin.system.exitProcess
 
 fun main(vararg args: String) {
+  println("Version=${getVersionFromResources()}")
   val cli = CommandLine(SortCommand())
   val exitCode = cli.execute(*args)
   exitProcess(exitCode)
+}
+
+fun getVersionFromResources(): String? {
+  val versionKeyValue = (Any::class as Any).javaClass.getResourceAsStream("/appinfo.properties")
+    ?.bufferedReader()
+    ?.readLines()
+    ?.first()
+
+  return versionKeyValue?.substringAfter("=")
 }
