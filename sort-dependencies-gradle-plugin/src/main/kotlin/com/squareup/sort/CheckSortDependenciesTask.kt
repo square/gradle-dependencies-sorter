ec751e01ce35d9096d563796263cb2cb3994d46f
Michael Glanznig <mglanznig@eurofunk.com>
2024-10-14 13:14:54 +0200

    Throw VerificationException on unsorted dependencies.

--- sort-dependencies-gradle-plugin/src/main/kotlin/com/squareup/sort/CheckSortDependenciesTask.kt
+++ sort-dependencies-gradle-plugin/src/main/kotlin/com/squareup/sort/CheckSortDependenciesTask.kt
@@ -38,6 +38,7 @@ abstract class CheckSortDependenciesTask @Inject constructor(
     val version = VersionNumber.parse(version.get().removeSuffix("-SNAPSHOT"))
 
     val result = execOps.exec { execSpec ->
+      execSpec.setIgnoreExitValue(true)
       execSpec.commandLine = listOf(
         "java",
         "-cp", sortProgram.asPath,
@@ -70,5 +71,13 @@ abstract class CheckSortDependenciesTask @Inject constructor(
     outputDirectory.asFile.get().mkdirs()
     val outputPath : Path = Paths.get(outputDirectory.asFile.get().absolutePath, "result.txt")
     File(outputPath.toUri()).writeText(resultText)
+
+    if (result.exitValue == 2) {
+      throw VerificationException("Dependencies are not correctly sorted.")
+    } else if (result.exitValue == 3) {
+      throw RuntimeException("There were parse errors.")
+    } else if (result.exitValue > 0) {
+      throw RuntimeException("The command failed with exit code ${result.exitValue}.")
+    }
   }
 }
